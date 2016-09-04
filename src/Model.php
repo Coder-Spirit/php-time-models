92c41d88232c292626af2ebca714ceba8a251907
Andreu Correa Casablanca <castarco@gmail.com>
2016-09-04 03:14:03 +0200

    First commit

---
+++ src/Model.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Litipk\MacPhply;
+
+
+class Model
+{
+
+}
---
+++ tests/ModelTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Litipk\MacPhply\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+
+class ModelTest extends TestCase
+{
+    public function testClassExistence()
+    {
+        $model = new \Litipk\MacPhply\Model();
+    }
+}
